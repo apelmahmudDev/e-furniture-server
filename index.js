dff55c12784cc1945b8d1feae0a5dbf992d06d80
Apel Mahmud <apelmahmud.dev@gmail.com>
2022-07-09 19:02:48 +0600

    fix: port number

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const port = process.env.DB_PORT || 8080;
+const port = process.env.PORT || 5000;
 
 const corsOptions = {
 	origin: "*",
