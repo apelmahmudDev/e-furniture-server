5852ea10fa6c049b6b4474b139be6ecfd3312c9c
Apel Mahmud <apelmahmud.dev@gmail.com>
2022-10-04 19:48:09 +0600

    feat: ✨ add timestamp for add product

--- model/product.schema.js
+++ model/product.schema.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
 
 // setup schema
-const productSchema = mongoose.Schema({
-	name: String,
-	price: String | Number,
-	description: String,
-	image: String,
-	category: String,
-	subCategory: {
-		type: String,
-		required: true,
+const productSchema = mongoose.Schema(
+	{
+		name: String,
+		price: String | Number,
+		description: String,
+		image: String,
+		category: String,
+		subCategory: {
+			type: String,
+			required: true,
+		},
 	},
-});
+	{ timestamps: true }
+);
 
 // export product model
 const Product = (module.exports = mongoose.model("product", productSchema));
